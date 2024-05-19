8d496f4ce4fbac96e68712d573ed8421a9f01365
nkluba <146091133+nkluba@users.noreply.github.com>
2024-05-19 09:13:23 +0300

    overall word count

--- epub_word_count.py
+++ epub_word_count.py
@@ -17,17 +17,21 @@ def get_chapter_text(item):
 def main(epub_file):
     book = epub.read_epub(epub_file)
     chapter_word_counts = {}
+    total_word_count = 0
 
     for item in book.get_items():
         if item.get_type() == ebooklib.ITEM_DOCUMENT and item.get_name().startswith('ch'):
             chapter_count = get_chapter_text(item)
             chapter_word_counts[item.get_name()] = chapter_count
+            total_word_count += chapter_count
 
     sorted_chapters = sorted(chapter_word_counts.items())
 
     for chapter, count in sorted_chapters:
         print(f"{chapter}: {count} words")
 
+    print(f"Total word count: {total_word_count} words")
+
 if __name__ == "__main__":
-    epub_file = '2.epub'
+    epub_file = '1.epub'
     main(epub_file)

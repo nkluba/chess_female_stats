4f9b6c05825c778a61f18fb504cb5cb942b100bf
Liubov <your_email@example.com>
2024-08-12 08:29:16 +0300

    black formatting

--- list_chess_tournaments.py
+++ list_chess_tournaments.py
@@ -18,26 +18,32 @@ def get_player_html(url):
     response = requests.get(url)
     return response.content
 
+
 def fix_headers(headers):
     """Fixes headers by adding the 'Link' column."""
     if "Link" not in headers:
         # Find the appropriate index to insert the 'Link' column
-        index_to_insert = next((i for i, header in enumerate(headers) if "FideID" in header), None)
+        index_to_insert = next(
+            (i for i, header in enumerate(headers) if "FideID" in header), None
+        )
         if index_to_insert is not None:
             headers.insert(index_to_insert + 1, "Link")
     return headers
 
+
 def parse_table(html_content):
     """Parses HTML content and extracts table data."""
     soup = BeautifulSoup(html_content, "html.parser")
-    title = soup.title.string.strip().replace("Chess-Results_Server_Chess-results.com_-_", "")
+    title = soup.title.string.strip().replace(
+        "Chess-Results_Server_Chess-results.com_-_", ""
+    )
     print(title)
 
-    #if "woman" in title.lower() or "girl" in title.lower() or "female" in title.lower():
+    # if "woman" in title.lower() or "girl" in title.lower() or "female" in title.lower():
     #    return None, None, None
 
     table = soup.find("table", class_="CRs1")
-    
+
     headers = [header.text.strip() for header in table.find_all("th")]
     headers = fix_headers(headers)
 
@@ -58,15 +64,25 @@ def parse_table(html_content):
 
 def extract_info_from_html(link):
     response = requests.get(link)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    profile_info = soup.find('div', class_='profile-top-info')
+    soup = BeautifulSoup(response.text, "html.parser")
+    profile_info = soup.find("div", class_="profile-top-info")
 
     if profile_info is not None:
-        federation = profile_info.find_all('div', class_='profile-top-info__block__row__data')[1].text.strip()
-        b_year = profile_info.find_all('div', class_='profile-top-info__block__row__data')[3].text.strip()
-        sex = profile_info.find_all('div', class_='profile-top-info__block__row__data')[4].text.strip()
-        fide_title = profile_info.find_all('div', class_='profile-top-info__block__row__data')[5].text.strip()
-        world_rank = profile_info.find('div', class_='profile-top-info__block__row__data').text.strip()
+        federation = profile_info.find_all(
+            "div", class_="profile-top-info__block__row__data"
+        )[1].text.strip()
+        b_year = profile_info.find_all(
+            "div", class_="profile-top-info__block__row__data"
+        )[3].text.strip()
+        sex = profile_info.find_all("div", class_="profile-top-info__block__row__data")[
+            4
+        ].text.strip()
+        fide_title = profile_info.find_all(
+            "div", class_="profile-top-info__block__row__data"
+        )[5].text.strip()
+        world_rank = profile_info.find(
+            "div", class_="profile-top-info__block__row__data"
+        ).text.strip()
         print(federation, b_year, sex, fide_title, world_rank)
         return federation, b_year, sex, fide_title, world_rank
     else:
@@ -74,14 +90,15 @@ def extract_info_from_html(link):
 
 
 def parse_fide_data(df):
-    if 'Link' in df.columns and not df['Link'].isnull().all():
+    if "Link" in df.columns and not df["Link"].isnull().all():
         print(df)
-    # Apply the function only to rows where 'Link' is not None
-        df = df.dropna(subset=['Link'])
+        # Apply the function only to rows where 'Link' is not None
+        df = df.dropna(subset=["Link"])
         # remove rows with wrong links. Sample (CZE instead of link): 45  45 Metastasio Niccolo CZE 0 None
-        df = df[df['Link'].str.startswith('http')]
-        df[['Federation', 'Birth Year', 'Sex', 'FIDE Title', 'World Rank']] = \
-            df['Link'].apply(lambda x: pd.Series(extract_info_from_html(x)))
+        df = df[df["Link"].str.startswith("http")]
+        df[["Federation", "Birth Year", "Sex", "FIDE Title", "World Rank"]] = df[
+            "Link"
+        ].apply(lambda x: pd.Series(extract_info_from_html(x)))
         return df
     else:
         return None
@@ -89,28 +106,29 @@ def parse_fide_data(df):
 
 def create_dataframe(headers, data):
     """Creates a DataFrame from table headers and data."""
-    try: 
-        df = pd.DataFrame(data, columns=headers).iloc[1: , :]
+    try:
+        df = pd.DataFrame(data, columns=headers).iloc[1:, :]
         return df
     except Exception as e:
         print("File can't be processed:", headers, data[:50])
         return pd.DataFrame()
-    # Else for team championships with other 
-    # File can't be processed: ['Rk.', '', 'Team', '1b', '2b', '3b', '4b', '5b', '6b', 'TB1', 'TB2', 'TB3'] 
-    # [['Rk.', '', 'Team', '1a', '1b', '2a', '2b', '3a', '3b', '4a', '4b', '5a', '5b', '6a', '6b', 'TB1', 'TB2', 'TB3'], 
+    # Else for team championships with other
+    # File can't be processed: ['Rk.', '', 'Team', '1b', '2b', '3b', '4b', '5b', '6b', 'TB1', 'TB2', 'TB3']
+    # [['Rk.', '', 'Team', '1a', '1b', '2a', '2b', '3a', '3b', '4a', '4b', '5a', '5b', '6a', '6b', 'TB1', 'TB2', 'TB3'],
     # ['1', '', 'Germany I', '*', '*', '1', '1½', '1½', '2', '1½', '1½', '1½', '2', '2', '1½', '19', '16', '0']
-    # 
+    #
 
-def process_url(url, save_path = "processed_data"):
+
+def process_url(url, save_path="processed_data"):
     html_content = get_player_html(url)
     headers, table_data, title = parse_table(html_content)
 
     if table_data is not None:
         df = create_dataframe(headers, table_data)
-        if 'Link' in df.columns:
+        if "Link" in df.columns:
             print(df)
             df = parse_fide_data(df)
-            df = df[df['Link'].notna()]
+            df = df[df["Link"].notna()]
             filename = f"{title.replace(' ', '_')}.csv"
             df.to_csv(os.path.join(save_path, filename), index=False)
 
@@ -129,7 +147,8 @@ def accept_cookies(driver):
     """Accept cookies on the website if the dialog appears."""
     try:
         agree_button = WebDriverWait(driver, 5).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "button.css-47sehv")))
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "button.css-47sehv"))
+        )
         agree_button.click()
     except Exception as e:
         print(f"Could not find the cookies acceptance button: {e}")
@@ -138,14 +157,19 @@ def accept_cookies(driver):
 def set_tournament_and_dates(driver, query):
     """Set the tournament search criteria and dates."""
     tournament_input = WebDriverWait(driver, 5).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "input[aria-labelledby='P1_lb_bez']")))
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, "input[aria-labelledby='P1_lb_bez']")
+        )
+    )
     tournament_input.clear()
     tournament_input.send_keys(query)
 
     start_date_input = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable((By.ID, "P1_txt_von_tag")))
+        EC.element_to_be_clickable((By.ID, "P1_txt_von_tag"))
+    )
     end_date_input = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable((By.ID, "P1_txt_bis_tag")))
+        EC.element_to_be_clickable((By.ID, "P1_txt_bis_tag"))
+    )
 
     start_date_input.clear()
     start_date_input.send_keys("01.01.2008")
@@ -156,7 +180,8 @@ def set_tournament_and_dates(driver, query):
 def set_max_results(driver, value):
     """Set the maximum number of result lines in the search."""
     max_lines_dropdown = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable((By.ID, "P1_combo_anzahl_zeilen")))
+        EC.element_to_be_clickable((By.ID, "P1_combo_anzahl_zeilen"))
+    )
     Select(max_lines_dropdown).select_by_value(value)
 
 
@@ -165,7 +190,7 @@ def get_tournament_links(driver):
     links = []
     elements = driver.find_elements(By.CSS_SELECTOR, "table.CRs2 a")
     for element in elements:
-        links.append(element.get_attribute('href'))
+        links.append(element.get_attribute("href"))
     return links
 
 
@@ -181,7 +206,9 @@ def search_and_collect_data(driver, query):
     set_tournament_and_dates(driver, query)
     set_max_results(driver, "5")  # Assuming '5' corresponds to '2000'
     WebDriverWait(driver, 5).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "input[aria-labelledby='P1_lb_bez']"))
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, "input[aria-labelledby='P1_lb_bez']")
+        )
     ).send_keys(Keys.ENTER)
     return get_tournament_links(driver)
 
@@ -195,21 +222,21 @@ def main():
     for query in queries:
         print("Processing query:", query)
 
-        csv_filename = query + '.csv'
+        csv_filename = query + ".csv"
         if os.path.exists(csv_filename) == False:
             links = search_and_collect_data(driver, query)
             print("Tournament links for query", query, ":", links)
 
-            df = pd.DataFrame({'Link': links, 'Checked': False})
+            df = pd.DataFrame({"Link": links, "Checked": False})
             df.to_csv(csv_filename, index=False)
-        
+
         else:
             df = pd.read_csv(csv_filename)
-            links = df.loc[df['Checked'] == False, 'Link'].tolist()
+            links = df.loc[df["Checked"] == False, "Link"].tolist()
 
         for link in links:
             process_url(link)
-            df.loc[df['Link'] == link, 'Checked'] = True
+            df.loc[df["Link"] == link, "Checked"] = True
             df.to_csv(csv_filename, index=False)
 
     driver.quit()
